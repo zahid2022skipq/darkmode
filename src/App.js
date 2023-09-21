4678a739f6e4df947fc9bd4476a9274409d6997a
zahid2022skipq <zahid.ali.skipq@gmail.com>
2023-09-21 11:52:20 +0500

    completed project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isDark, setDark] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${isDark ? "dark" : "light"}`}>
+      <button onClick={() => setDark(true)} className='btn-dark' >Dark</button>
+      <button onClick={() => setDark(false)} className='btn-light'>Ligh</button>
     </div>
   );
 }
